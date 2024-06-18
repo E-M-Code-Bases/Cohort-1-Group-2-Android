851b85e4ccee4a1b255a800132bbc115233987df
Norpha <normanndiritu@gmail.com>
2024-06-18 12:06:28 +0300

    Retrfoit added, movie service added, Upcoming Movie Data Class Added, Reposiotory added, respective gradle dependencies added

--- app/src/main/java/com/example/movies_poa_app/repository/MovieRepository.kt
+++ app/src/main/java/com/example/movies_poa_app/repository/MovieRepository.kt
@@ -1,4 +1,11 @@
 package com.example.movies_poa_app.repository
 
-class MovieRepository {
+import com.example.movies_poa_app.retrofit.MovieService
+
+
+class MovieRepository(private val service: MovieService) {
+    suspend fun getUpcoming(apiKey: String) = service.getUpcoming(apiKey)
+
 }
+
+
---
+++ app/src/main/java/com/example/movies_poa_app/retrofit/MovieService.kt
@@ -0,0 +1,14 @@
+package com.example.movies_poa_app.retrofit
+
+import com.example.movies_poa_app.model.UpcomingMovie
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface MovieService {
+
+    @GET("/movie/upcoming")
+    suspend fun getUpcoming(@Query("c86b2436b1121f1894caf99d7c17452d") apiKey: String): UpcomingMovie
+
+
+
+}
