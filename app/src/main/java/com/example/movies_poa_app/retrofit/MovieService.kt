49992a0f51d8cdd94ef5a070e3c198c8d31d12c4
Norpha <normanndiritu@gmail.com>
2024-06-18 13:10:25 +0300

    UpcomingMovie ViewModel Added

--- app/src/main/java/com/example/movies_poa_app/retrofit/MovieService.kt
+++ app/src/main/java/com/example/movies_poa_app/retrofit/MovieService.kt
@@ -1,5 +1,6 @@
 package com.example.movies_poa_app.retrofit
 
+import com.example.movies_poa_app.model.MovieResponse
 import com.example.movies_poa_app.model.UpcomingMovie
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -7,7 +8,7 @@ import retrofit2.http.Query
 interface MovieService {
 
     @GET("/movie/upcoming")
-    suspend fun getUpcoming(@Query("c86b2436b1121f1894caf99d7c17452d") apiKey: String): UpcomingMovie
+    suspend fun getUpcoming(@Query("c86b2436b1121f1894caf99d7c17452d") apiKey: String): MovieResponse
 
 
 
