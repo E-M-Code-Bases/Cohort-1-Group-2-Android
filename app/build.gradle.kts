806862d8c2def20c02d241ca906ac541f86aad01
Kenneth Owino <kenkunde94@gmail.com>
2024-06-19 09:10:43 +0300

    updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,7 @@ android {
     buildFeatures{
         dataBinding = true
     }
+
 }
 
 dependencies {
@@ -59,20 +60,23 @@ dependencies {
 //    implementation ("androidx.appcompat:appcompat:1.3.1")
 //    implementation ("com.google.android.material:material:1.4.0")
 //    implementation ("androidx.constraintlayout:constraintlayout:2.1.0")
-//    implementation ("androidx.lifecycle:lifecycle-extensions:2.2.0")
-//    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.1")
-//    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.3.1")
+    implementation ("androidx.lifecycle:lifecycle-extensions:2.2.0")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.0")
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.4.0")
 //    implementation ("androidx.navigation:navigation-fragment-ktx:2.3.5")
 //    implementation ("androidx.navigation:navigation-ui-ktx:2.3.5")
 //    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.1")
 //    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.1")
 //    implementation ("org.koin:koin-android:3.1.2")
 //    implementation ("org.koin:koin-androidx-viewmodel:3.1.2")
-//    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
-//    implementation ("com.squareup.retrofit2:converter-moshi:2.11.0")
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-moshi:2.11.0")
 //    implementation ("com.squareup.okhttp3:logging-interceptor:4.9.1")
     implementation ("com.google.android.material:material:1.4.0")
     implementation ("androidx.viewpager2:viewpager2:1.0.0")
     implementation ("androidx.fragment:fragment-ktx:1.3.6")
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    implementation ("io.insert-koin:koin-android:3.1.2")
+    implementation ("io.insert-koin:koin-androidx-viewmodel:3.1.2")
 
 }
--- app/src/main/java/com/example/movies_poa_app/repository/MovieRepository.kt
+++ app/src/main/java/com/example/movies_poa_app/repository/MovieRepository.kt
@@ -2,3 +2,4 @@ package com.example.movies_poa_app.repository
 
 class MovieRepository {
 }
+
