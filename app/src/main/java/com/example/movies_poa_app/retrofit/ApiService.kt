86ba43ec150208b46d092f4a943edb07d3c99087
Jemutai <janetjemutai19@gmail.com>
2024-06-19 12:06:48 +0300

    MovieAppModule,Retrofit client refactored

--- app/src/main/java/com/example/movies_poa_app/retrofit/ApiService.kt
+++ app/src/main/java/com/example/movies_poa_app/retrofit/ApiService.kt
@@ -4,11 +4,8 @@ import com.example.movies_poa_app.model.UpcomingMovie
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-interface MovieService {
+interface ApiService {
 
     @GET("/movie/upcoming")
     suspend fun getUpcoming(@Query("c86b2436b1121f1894caf99d7c17452d") apiKey: String): UpcomingMovie
-
-
-
 }
