b5d480023d690779dc348f35a110a336f48f02fc
Jemutai <janetjemutai19@gmail.com>
2024-06-19 15:36:02 +0300

    POST & GET favourites movies added

--- app/src/main/java/com/example/movies_poa_app/model/Movie.kt
+++ app/src/main/java/com/example/movies_poa_app/model/Movie.kt
@@ -1,9 +1,21 @@
 package com.example.movies_poa_app.model
 
-data class UpcomingMovie(
+import com.squareup.moshi.Json
+
+data class Movie(
     val id: Int,
     val original_title: String,
-    val overview: String,
-    val release_date: String,
-    val poster_path: String
+    @Json(name = "poster_path") val posterPath: String?,
+    @Json(name = "release_date") val releaseDate: String?,
+    val overview: String
 )
+
+
+
+data class FavoriteRequest(
+    val media_type: String,
+    val media_id: Int,
+    val favorite: Boolean
+)
+
+data class MovieResponse (val results: List<Movie>)
--- app/src/main/java/com/example/movies_poa_app/model/MovieResponse.kt
+++
@@ -1,3 +0,0 @@
-package com.example.movies_poa_app.model
-
-data class MovieResponse (val results: List<UpcomingMovie>)
