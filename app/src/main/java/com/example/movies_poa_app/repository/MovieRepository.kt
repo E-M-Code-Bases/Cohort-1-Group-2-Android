44b86d7c1f265d0aa184c8b530bee17d73c8048a
Jemutai <janetjemutai19@gmail.com>
2024-06-19 16:21:58 +0300

    getFavouriteItem Method added

--- app/src/main/java/com/example/movies_poa_app/repository/MovieRepository.kt
+++ app/src/main/java/com/example/movies_poa_app/repository/MovieRepository.kt
@@ -1,13 +1,18 @@
 package com.example.movies_poa_app.repository
 
 
+import com.example.movies_poa_app.model.MovieResponse
 import com.example.movies_poa_app.retrofit.ApiService
+import retrofit2.Response
 
 
 class MovieRepository(private val service: ApiService) {
     suspend fun getUpcoming(apiKey: String) = service.getUpcoming(apiKey)
 
+    suspend fun getFavoriteMovies(accountId: Int, apiKey: String, sessionId: String): Response<MovieResponse> {
+        return service.getFavoriteMovies(accountId, apiKey, sessionId)
 
+    }
 
 }
 
