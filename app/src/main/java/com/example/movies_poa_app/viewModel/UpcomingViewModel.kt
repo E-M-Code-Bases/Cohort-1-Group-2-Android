eddff46dd167044cd4d651c126f24cb405a0eac0
Norpha <normanndiritu@gmail.com>
2024-06-24 09:18:55 +0300

    upcoming movies updated

--- app/src/main/java/com/example/movies_poa_app/viewModel/UpcomingViewModel.kt
+++ app/src/main/java/com/example/movies_poa_app/viewModel/UpcomingViewModel.kt
@@ -1,4 +1,4 @@
-//package com.example.movies_poa_app.viewModel
+package com.example.movies_poa_app.viewModel//package com.example.movies_poa_app.viewModel
 //
 //import androidx.lifecycle.ViewModel
 //import androidx.lifecycle.viewModelScope
