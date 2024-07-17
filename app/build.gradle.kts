069943ee104853e43ba2ecc925a804ece08deccc
Dorcas <dorcasjemosop@gmail.com>
2024-07-17 15:57:06 +0300

    updated changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,23 +1,23 @@
+
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+    id("kotlin-kapt")
+    id("androidx.navigation.safeargs.kotlin")
 }
 
 android {
-    namespace = "com.jemutai.androidapp"
+    namespace = "com.example.movies_poa_app"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.jemutai.androidapp"
+        applicationId = "com.example.movies_poa_app"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary = true
-        }
     }
 
     buildTypes {
@@ -37,21 +37,24 @@ android {
         jvmTarget = "1.8"
     }
     buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
+        viewBinding = true
+        dataBinding = true
+
     }
 }
 
 dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.legacy.support.v4)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.fragment.ktx)
+    implementation(libs.androidx.paging.common.android)
 
-<<<<<<< Updated upstream
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -65,7 +68,7 @@ dependencies {
 
     //implementation("io.insert-koin:koin-android:3.1.2")
     //implementation("io.insert-koin:koin-androidx-viewmodel:3.1.2")
-   // implementation("io.insert-koin:koin-core:3.3.2")
+    // implementation("io.insert-koin:koin-core:3.3.2")
     //implementation("io.insert-koin:koin-androidx:3.4.0")
 
     implementation ("androidx.navigation:navigation-fragment-ktx:2.5.3") // or the latest version
@@ -80,23 +83,3 @@ dependencies {
     implementation("com.squareup.picasso:picasso:2.71828")
     implementation ("com.pierfrancescosoffritti.androidyoutubeplayer:core:12.0.0")
 }
-=======
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    implementation("androidx.activity:activity-compose:1.8.2")
-    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    implementation("androidx.compose.material3:material3-android:1.2.1")
-    implementation("androidx.compose.material3:material3-desktop:1.2.1")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
-}
->>>>>>> Stashed changes
