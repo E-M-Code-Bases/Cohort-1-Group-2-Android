4ac503976d9867f0c2b53e6f282ac31533d31bcf
Elizabeth Tabitha <155945625+Elizabethtabitha1310@users.noreply.github.com>
2024-08-12 15:32:26 -0700

    popular movies

--- app/src/main/java/com/example/movies_poa_app/adapters/PopularAdapter.kt
+++ app/src/main/java/com/example/movies_poa_app/adapters/PopularAdapter.kt
@@ -1,18 +1,39 @@
 package com.example.movies_poa_app.adapters
 
+import android.os.Bundle
 import  com.example.movies_poa_app.model.Movie
 import android.view.LayoutInflater
+import com.squareup.picasso.Picasso
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.example.movies_poa_app.R
 import com.example.movies_poa_app.databinding.ItemMovieBinding
 
-class MoviesAdapter : RecyclerView.Adapter<MoviesAdapter.MovieViewHolder>() {
+class PopularAdapter : RecyclerView.Adapter<PopularAdapter.MovieViewHolder>() {
+
     class MovieViewHolder(private val binding: ItemMovieBinding) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(movie:Movie) {
+
+        fun bind(movie: Movie) {
             binding.movieTitle.text = movie.original_title
             binding.description.text = movie.overview
+
+            val posterUrl = "https://image.tmdb.org/t/p/w500${movie.poster_path}"
+
+            Picasso.get()
+                .load(posterUrl)
+                .placeholder(R.drawable.ic_launcher_background)
+                .into(binding.moviePoster)
+
+            binding.root.setOnClickListener{
+                val bundle = Bundle().apply {
+                    putSerializable("movie", movie)
+                }
+                binding.root.findNavController().navigate(R.id.action_singleMovieFragment_to_playFragment, bundle)
+
+            }
         }
     }
   private val diff =  object : DiffUtil.ItemCallback<Movie>() {
@@ -31,7 +52,7 @@ class MoviesAdapter : RecyclerView.Adapter<MoviesAdapter.MovieViewHolder>() {
         return MovieViewHolder(binding)
     }
 
-    override fun getItemCount() = 0
+    override fun getItemCount() = moviesList.currentList.size
 
     override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
         val movie = moviesList.currentList[position]
--- app/src/main/java/com/example/movies_poa_app/view/fragments/PopularFragment.kt
+++ app/src/main/java/com/example/movies_poa_app/view/fragments/PopularFragment.kt
@@ -3,54 +3,60 @@ package com.example.movies_poa_app.view.fragments
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.movies_poa_app.databinding.FragmentPopularBinding
-import com.example.movies_poa_app.adapters.MoviesAdapter
+import com.example.movies_poa_app.adapters.PopularAdapter
+import com.example.movies_poa_app.model.MovieResponse
+import com.example.movies_poa_app.repository.MovieRepository
+import com.example.movies_poa_app.retrofit.AppModule
 import com.example.movies_poa_app.viewModel.PopularViewModel
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class PopularFragment : Fragment() {
 
     private lateinit var binding: FragmentPopularBinding
-    private val viewModel: PopularViewModel by viewModels()
-    private lateinit var adapter: MoviesAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentPopularBinding.inflate(inflater, container, false)
-        binding.viewModel = viewModel
-        binding.lifecycleOwner = this
-        adapter = MoviesAdapter()
 
-        viewModel.popularMovies.observe(viewLifecycleOwner, Observer { movies ->
-            adapter.moviesList.submitList(movies)
-        })
 
-        setupRecyclerView()
-        setupSearch()
+        val adp = PopularAdapter()
 
+        binding.recyclerViewTopRated.apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = adp
+        }
+
+        val service = AppModule().apiService()
+
+        val repo = MovieRepository(service)
+
+        val viewModel: PopularViewModel by viewModels {
+            PopularViewModel.PopularProvider(repo)
+        }
+        val cont = requireContext()
+        viewModel.popularMovies.observe(viewLifecycleOwner) { movies ->
+            Log.d("movies", movies.toString())
+            adp.moviesList.submitList(movies)
+
+            binding.recyclerViewTopRated.apply {
+                layoutManager = LinearLayoutManager(cont)
+                adapter = adp
+            }
+        }
         return binding.root
     }
 
-    private fun setupRecyclerView() {
-        binding.recyclerViewTopRated.layoutManager = LinearLayoutManager(context)
-        binding.recyclerViewTopRated.adapter = adapter
-    }
-
-    private fun setupSearch() {
-        binding.editTextSearch.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
-            override fun afterTextChanged(s: Editable?) {
-                viewModel.searchMovies(s.toString())
-            }
-        })
-    }
 }
+
